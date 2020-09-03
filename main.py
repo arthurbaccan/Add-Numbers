3c78df1ba4c4946bffe503eae2dfa2ba620fce5c
arthurbaccan <70720781+arthurbaccan@users.noreply.github.com>
2020-09-03 16:24:55 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 # This is a sample Python script.
 from termcolor import cprint
 
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 def Sum(*nums):
     global num
     nums = list(nums)
@@ -16,14 +12,14 @@ def Sum(*nums):
     else:
         for c in range(count):
             result += float(num[c])
-    cprint(f"resultado = {result}", "green")
+    cprint(f"result = {result}", "green")
 
 
 def get_number_input():
     global numbers
     numbers = []
     number = ""
-    cprint("Digite '=' para mostrar o resultado da adição", 'yellow')
+    cprint("Type '=' to show the result of the addition", 'yellow')
     while True:
 
         number = str(input("Digite um número: "))
@@ -33,16 +29,13 @@ def get_number_input():
             try:
                 float(number)
             except ValueError:
-                cprint("Número ou comando inválido\nA resposta não foi contada", "red")
+                cprint("Invalid command!\nThis command didin't affect your addition", "red")
                 cprint("-=" * 20, "blue")
             else:
                 numbers.append(number)
     return numbers
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     numbers = get_number_input()
     Sum(numbers)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
