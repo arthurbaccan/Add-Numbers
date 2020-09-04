6b2a830dbff1772de80dcd960bebaa0556d0306e
arthurbaccan <70720781+arthurbaccan@users.noreply.github.com>
2020-09-04 09:50:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,55 @@
 # This is a sample Python script.
 from termcolor import cprint
 
+
+# Function for addition of unlimited numbers
 def Sum(*nums):
     global num
     nums = list(nums)
     # print(nums)
     count = 0
     result = 0
+    # check each numbers in the list of inputs and count it's lenght
     for num in nums:
         count += len(num)
     else:
+        # make an addition with each number in the list of inputs
         for c in range(count):
             result += float(num[c])
-    cprint(f"result = {result}", "green")
+    cprint(f"result of the addition = {result}", "green")
 
 
+# function for getting number inputs
 def get_number_input():
     global numbers
     numbers = []
-    number = ""
-    cprint("Type '=' to show the result of the addition", 'yellow')
+    cprint("Type '=' to show the addition result", 'yellow')
     while True:
-
-        number = str(input("Digite um número: "))
+        # number variable gets a string input
+        number = str(input("Type a number: "))
+        # break the input loop if "=" is typed
         if number == "=" or number == "==":
             break
         else:
+            # check to see if a number was typed
             try:
                 float(number)
+            # if a there is ValueError error, the command is invalid and the input has letters or etc...
             except ValueError:
-                cprint("Invalid command!\nThis command didin't affect your addition", "red")
+                cprint("Invalid number or command\nThe answer didn't affect your addition", "red")
                 cprint("-=" * 20, "blue")
+            # if everything goes ok, it adds the input to a number input list
             else:
                 numbers.append(number)
+    # returns a list with numbers
     return numbers
 
+
+# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    # call the function for getting unlimited number inputs
     numbers = get_number_input()
+    # call the function to make an addition with the number inputs
     Sum(numbers)
 
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
